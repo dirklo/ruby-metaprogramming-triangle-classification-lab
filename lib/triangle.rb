df77af4cb93ea6f0a97df0c0181d55e5e99cee6b
Rick <dirklo@gmail.com>
2020-12-23 13:07:10 +0100

    commit tree 1f8b5095f9b87163e55f2f2234b08083c1ffed3b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def valid?
+    side1 = self.side1
+    side2 = self.side2
+    side3 = self.side3
+
+    case
+      when side1 <= 0 || side2 <= 0 || side3 <= 0 then false
+      when side1 + side2 <= side3 then false
+      when side2 + side3 <= side1 then false
+      when side3 + side1 <= side2 then false
+      else true
+    end
+  end
+
+  def kind
+    raise TriangleError if !self.valid?
+    
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end  
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
